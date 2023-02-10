41fe89a066d6d3ce8af6cba91f8d683ada6b6897
Andreas Östling <andreas.ostling@ur.se>
2023-02-10 15:29:44 +0100

    add debugger, cheerio and religiousOrdersFixture

---
+++ src/__fixtures__/religiousOrders.ts
@@ -0,0 +1,211 @@
+const religiousOrders = {
+  "Mendicant orders": [
+    {
+      officialName: "Ordo Augustiniensium Discalceatorum",
+      acronym: "O.A.D.",
+      name: "Discalced Augustinians",
+    },
+    {
+      officialName: "Ordo Minimorum",
+      acronym: "O.M.",
+      name: "Minims",
+    },
+    {
+      officialName: "Ordo Augustinianorum Recollectorum",
+      acronym: "O.A.R.",
+      name: "Augustinians Recollects",
+    },
+    {
+      officialName: "Ordo Fratrum Discalceatorum B. Mariae V. de Monte Carmelo",
+      acronym: "O.C.D.",
+      name: "Discalced Carmelites",
+    },
+    {
+      officialName: "Ordo Servorum Mariae",
+      acronym: "O.S.M.",
+      name: "Servites",
+    },
+    {
+      officialName: "Ordo Fratrum Minorum",
+      acronym: "O.F.M.",
+      name: "Franciscans",
+    },
+    {
+      officialName: "Ordo Fratrum Minorum Capuccinorum",
+      acronym: "O.F.M. Cap.",
+      name: "Capuchin Franciscans",
+    },
+    {
+      officialName: "Ordo Fratrum Minorum Conventualium",
+      acronym: "O.F.M. Conv.",
+      name: "Conventual Franciscans",
+    },
+    {
+      officialName: "Ordo Fratrum Praedicatorum",
+      acronym: "O.P.",
+      name: "Dominicans",
+    },
+    {
+      officialName: "Ordo B. Mariae Virginis de Mercede",
+      acronym: "O. de M.",
+      name: "Mercedarians",
+    },
+    {
+      officialName: "Ordo Fratrum Beatissimae Mariae Virginis de Monte Carmelo",
+      acronym: "O. Carm.",
+      name: "Carmelites",
+    },
+    {
+      officialName: "Ordo Fratrum Sancti Augustini",
+      acronym: "O.S.A.",
+      name: "Augustinians",
+    },
+    {
+      officialName: "Ordo Ssmae Trinitatis",
+      acronym: "O.SS.T.",
+      name: "Trinitarians",
+    },
+    {
+      officialName: "Tertius Ordo Regularis S. Francisci",
+      acronym: "T.O.R.",
+      name: "Brothers of Penance",
+    },
+  ],
+  "Monastic Orders": [
+    {
+      officialName: "Ordo Cartusiensis",
+      acronym: "Cart.",
+      name: "Carthusians",
+    },
+    {
+      officialName: "Ordo Sancti Hieronymi",
+      acronym: "O.S.H.",
+      name: "Hieronymites",
+    },
+    {
+      officialName: "Ordo Cisterciensis",
+      acronym: "O. Cist.",
+      name: "Cistercians",
+    },
+    {
+      officialName: "Ordo Cisterciensis Strictioris Observantiae",
+      acronym: "O.C.S.O.",
+      name: "Trappist",
+    },
+    {
+      officialName: "Ordo Libanensis Maronitarum",
+      acronym: "O.L.M.",
+      name: "Baladites",
+    },
+    {
+      officialName: "Ordo S. Benedicti",
+      acronym: "O.S.B.",
+      name: "Benedictines",
+    },
+    {
+      officialName: "Ordo Basilianus S. Iosaphat",
+      acronym: "O.S.B.M.",
+      name: "Basilians",
+    },
+    {
+      officialName:
+        "Ordo Basilianus S. Iohannis Baptistae, Soaritarum Melkitarum",
+      acronym: "B.C.",
+      name: "&nbsp;",
+    },
+    {
+      officialName: "Ordo Fratrum S. Pauli Primi Eremitae",
+      acronym: "O.S.P.P.E.",
+      name: "Pauline Fathers",
+    },
+    {
+      officialName: "Ordo Basilianus Ssmi Salvatoris Melkitarum",
+      acronym: "B.S.",
+      name: "&nbsp;",
+    },
+    {
+      officialName: "Ordo Maronita Beatae Mariae Virginis",
+      acronym: "O.M.M.",
+      name: "&nbsp;",
+    },
+  ],
+  "Cleric Regulars": [
+    {
+      officialName: "Congregatio Clericorum Regularium S. Pauli",
+      acronym: "B.",
+      name: "Barnabites",
+    },
+    {
+      officialName: "Ordo Clericorum Regularium vulgo Theatinorum",
+      acronym: "C.R.",
+      name: "Theatines",
+    },
+    {
+      officialName: "Ordo Clericorum Regularium a Somascha",
+      acronym: "C.R.S.",
+      name: "&nbsp;",
+    },
+    {
+      officialName: "Ordo Clericorum Regularium Matris Dei",
+      acronym: "O. M. D.",
+      name: "&nbsp;",
+    },
+    {
+      officialName:
+        "Ordo Clericorum Regularium Pauperum Matris Dei Scholarum Piarum",
+      acronym: "Sch. P.",
+      name: "Piarists",
+    },
+    {
+      officialName: "Ordo Clericorum Regularium Ministrantium Infirmis",
+      acronym: "M.I.",
+      name: "Camilians",
+    },
+    {
+      officialName: "Societas Jesu",
+      acronym: "S.J.",
+      name: "Jesuits",
+    },
+  ],
+  "Canon Regulars": [
+    {
+      officialName: "Congregatio Ssmi Salvatoris Lateranensis",
+      acronym: "C.R.L.",
+      name: "&nbsp;",
+    },
+    {
+      officialName:
+        "Sacer et Apostolicus Ordo Canonicorum Regularium S. Augustini",
+      acronym: "C.R.S.A.",
+      name: "Canon Regulars",
+    },
+    {
+      officialName:
+        "Ordo Fratrum Domus Hospitalis Sanctae Mariae Teutonicorum in Jerusalem",
+      acronym: "O.T.",
+      name: "&nbsp;",
+    },
+    {
+      officialName: "Candidus et Canonicus Ordo Praemonstratensis",
+      acronym: "O. Praem.",
+      name: "Norbertines or Premonstratensians",
+    },
+    {
+      officialName: "Ordo Canonicorum Regularium Sancae Crucis",
+      acronym: "O.R.C.",
+      name: "&nbsp;",
+    },
+    {
+      officialName: "Congregatio Helvetica o Sancto Mauritio Agaunensis",
+      acronym: "C.R.A",
+      name: "&nbsp;",
+    },
+    {
+      officialName: "Canonici Regulares Ordinis S. Crucis",
+      acronym: "O.S.C.",
+      name: "Holy Cross",
+    },
+  ],
+};
+
+export default religiousOrders;
